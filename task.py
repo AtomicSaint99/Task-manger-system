74656ec11693c0b390dc1e57c9b4900d8719dc1e
Kingsley <kingkelvin474@gmail.com>
2026-09-12 15:00:41 -0400

    Added two functions

--- task.py
+++ task.py
@@ -5,3 +5,11 @@ def menu():
     print("2. View Tasks")
     print("3. Complete Task")
 
+def add_task():
+    task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added")
+
+def view_tasks():
+    for task in tasks:
+        print(task)
